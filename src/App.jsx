21860735957c26a478cfcf6f252725dc61562158
Dwarf_Painter <2301140014@ms.hanu.edu.vn>
2024-06-04 16:05:28 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,32 @@ import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors';
+import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle/>
+      <hr></hr>
       <div>Dwarf_Painter</div>
+      <Typography variant='body2' color='text.secondary'>Panhackmap02</Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
---
+++ src/theme.js
@@ -0,0 +1,22 @@
+import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+
+// Create a theme instance.
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange
+      }
+    },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange
+      }
+    }
+  }
+})
+
+export default theme
