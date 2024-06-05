4a7cc7ae9bf8cf9e76073e825d16a157704a22ad
Dwarf_Painter <2301140014@ms.hanu.edu.vn>
2024-06-05 19:46:03 +0700

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,11 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import Typography from '@mui/material/Typography'
-import { useColorScheme } from '@mui/material/styles'
+import Board from './pages/Boards/_id'
 
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
 
 function App() {
 
   return (
     <>
-      <ModeToggle/>
-      <hr></hr>
-      <div>Dwarf_Painter</div>
-      <Typography variant='body2' color='text.secondary'>Panhackmap02</Typography>
-
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br/>
-      <AccessAlarmIcon></AccessAlarmIcon>
-      <ThreeDRotation></ThreeDRotation>
-      <br/>
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
+      <Board/>
     </>
   )
 }
---
+++ src/components/AppBar/index.jsx
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box'
+import SelectMode from '../../components/ModeSelect'
+
+function AppBar() {
+  return (
+    <Box sx={{
+      width: '100%',
+      height: (theme) => theme.trello.appBarHeight,
+      backgroundColor: 'primary.light',
+      display: 'flex',
+      alignItems: 'center'
+    }}>
+      <SelectMode></SelectMode>
+    </Box>
+  )
+}
+
+export default AppBar
