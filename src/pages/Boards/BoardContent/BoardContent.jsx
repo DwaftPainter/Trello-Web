3c044e1af7825ebaec4653f40af20c653e5cfa4b
Dwarf_Painter <2301140014@ms.hanu.edu.vn>
2024-06-11 16:41:44 +0700

    Drag And Drop Columns

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,21 +1,57 @@
 import Box from '@mui/material/Box'
 import ListColumn from './ListColumns/ListColumn'
 import { mapOrder } from '~/utils/sorts'
+import {
+  DndContext,
+  // PointerSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
+import { useState, useEffect } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
 
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
+  const mySensor = useSensors(mouseSensor, touchSensor)
+
+  const [orderedColumnsState, setOrderedColumnsState] = useState([])
+
+  useEffect(() => {
+    setOrderedColumnsState(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event
+
+    if (!over) return
+    if (active.id !== over.id) {
+      const oldIndex = orderedColumnsState.findIndex(c => c._id === active.id)
+      const newIndex = orderedColumnsState.findIndex(c => c._id === over.id)
+
+      const dndOrderedColumns = arrayMove(orderedColumnsState, oldIndex, newIndex)
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      setOrderedColumnsState(dndOrderedColumns)
+    }
+
+  }
+
   return (
-    <Box sx={{
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      display: 'flex',
-      bgcolor: (theme) => ( theme.palette.mode === 'dark' ? '#34495e' : '#1976d2' ),
-      overflowX: 'auto',
-      overflowY: 'hidden',
-      p: '10px 0'
-    }}>
-      <ListColumn columns={orderedColumns}></ListColumn>
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={mySensor}>
+      <Box sx={{
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        display: 'flex',
+        bgcolor: (theme) => ( theme.palette.mode === 'dark' ? '#34495e' : '#1976d2' ),
+        overflowX: 'auto',
+        overflowY: 'hidden',
+        p: '10px 0'
+      }}>
+        <ListColumn columns={orderedColumnsState}></ListColumn>
+      </Box>
+    </DndContext>
   )
 }
 
