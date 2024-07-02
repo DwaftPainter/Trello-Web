ab46b4d399509ea9cf06c43896dd93f097b313f0
Dwarf_Painter <2301140014@ms.hanu.edu.vn>
2024-07-02 13:19:08 +0700

    Test Route

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ function Home() {
   return <div>Welcome to the Home Page</div>
 }
 
+function Test() {
+  return <div>This is test page</div>
+}
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
         <Route path="/boards/:boardId" element={<Board />} />
         {/* Optionally redirect all unmatched routes to the home page or a 404 page */}
         <Route path="*" element={<Navigate to="/" />} />
