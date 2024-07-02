3559c2f171830029ad4837503bbc55f966f5086b
Dwarf_Painter <2301140014@ms.hanu.edu.vn>
2024-07-02 12:57:16 +0700

    Config Vite

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-xzib.onrender.com'
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-xzib.onrender.com'
+}
+
+export const API_ROOT = apiRoot
